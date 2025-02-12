dbccbc3ac3e793b17d829f6523713c18f3a041fd
Harshit Rai <harshit143rai@gmail.com>
2025-02-12 09:22:46 +0530

    12 feb

--- CE/fileasync.js
+++ CE/fileasync.js
@@ -1,1 +1,17 @@
 const fs = require('fs');
+const read = async () =>{
+    const data = await fs.readFileSync("./data.txt", "utf8");
+    console.log(data);
+
+}
+const write = async () =>{
+    const newdata = "let me cook";
+    await fs.writeFileSync("./data1.txt", newdata);
+    console.log("File written successfully");
+}
+write();
+read();
+console.log(" first time Reading file...");
+console.log(" second time Reading file...");
+console.log(" third question Reading file...");
+console.log(" fourth question Reading file...");
